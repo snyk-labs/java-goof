bb41d6b3d68b002763c2b2f8204f2e17358a61a3
benas <md.benhassine@gmail.com>
2013-08-27 17:11:53 +0200

    update todolist data

--- todolist-core/src/main/resources/config/data.sql
+++ todolist-core/src/main/resources/config/data.sql
@@ -3,13 +3,13 @@ INSERT INTO user VALUES (1,'Foo', 'Bar','foo@bar.org', 'foobar');
 
 --todos
 
-INSERT INTO todo VALUES (1,1,'Print invoices', 1, 1,'2012-05-25');
-INSERT INTO todo VALUES (2,1,'Buy bread', 1, 1,'2012-02-02');
-INSERT INTO todo VALUES (3,1,'Remember the milk', 0, 2,'2012-10-11');
-INSERT INTO todo VALUES (4,1,'Change email password', 1, 2,'2012-07-25');
-INSERT INTO todo VALUES (5,1,'Record the speech', 0, 0,'2012-08-12');
-INSERT INTO todo VALUES (6,1,'Clean the Hall', 1, 0,'2012-08-12');
-INSERT INTO todo VALUES (7,1,'Read the article', 1, 2,'2012-10-10');
-INSERT INTO todo VALUES (8,1,'Change address', 0, 0,'2012-12-12');
-INSERT INTO todo VALUES (9,1,'Prepare the slides', 0, 1,'2012-04-30');
-INSERT INTO todo VALUES (10,1,'Organize a meeting with boss', 0, 2,'2012-08-22');
+INSERT INTO todo VALUES (1,1,'Prepare birthday party', 1, 1,'2014-05-25');
+INSERT INTO todo VALUES (2,1,'Wash the car', 1, 1,'2014-02-02');
+INSERT INTO todo VALUES (3,1,'Take out the trash', 0, 2,'2014-10-11');
+INSERT INTO todo VALUES (4,1,'Buy a gift for mom', 1, 2,'2014-07-25');
+INSERT INTO todo VALUES (5,1,'Remember the milk', 0, 0,'2014-08-12');
+INSERT INTO todo VALUES (6,1,'Prepare for interview', 1, 0,'2014-08-12');
+INSERT INTO todo VALUES (7,1,'Email David about project', 1, 2,'2014-10-10');
+INSERT INTO todo VALUES (8,1,'Feed the dog', 0, 0,'2014-12-12');
+INSERT INTO todo VALUES (9,1,'Change password', 0, 1,'2014-04-30');
+INSERT INTO todo VALUES (10,1,'Send weekly reports', 0, 2,'2014-08-22');
