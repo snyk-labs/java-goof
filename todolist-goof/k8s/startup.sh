9a7d17939d1f16f327d2d4caa9784a1c4dd2f0ab
Eric Smalling <eric.smalling@snyk.io>
2021-12-16 21:47:47 -0600

    putting it all in k8s

---
+++ todolist-goof/k8s/startup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+MYDIR=$(dirname $0)
+read -e -i "${DOCKER_ACCOUNT}" -p "Please enter your DockerHub user/account name: " input
+name="${input:-$DOCKER_ACCOUNT}"
+
+cat $MYDIR/java-goof.yaml | envsubst | kubectl apply -f -
+
+echo "⌚️ Waiting for pod deployment..."
+kubectl wait --for=condition=ready pod \
+             --selector=app=goof \
+             --timeout=90s
+
+kubectl port-forward service/goof 8000:80
