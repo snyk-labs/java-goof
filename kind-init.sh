c5ab612db513ad8c22e863cdfbd3530ef7576449
Eric Smalling <eric.smalling@snyk.io>
2021-12-22 09:54:40 -0600

    Cleanup of a few issues in k8s deploy plus beginning kind cluster setup

---
+++ kind-init.sh
@@ -0,0 +1,1 @@
+todolist-goof/k8s/kind-init.sh
--- log4shell-goof/log4shell-server/k8s/imagebuild.sh
+++ log4shell-goof/log4shell-server/k8s/imagebuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+MYDIR=$(dirname $0)
 if [[ "$1" == "" ]]; then
   read -e -i "${DOCKER_ACCOUNT}" -p "Please enter your DockerHub user/account name: " input
   name="${input:-$DOCKER_ACCOUNT}"
