2ad675e0839f25e0395a139e113358ef687f07d2
Eric <eric.smalling@snyk.io>
2022-04-20 19:19:06 -0500

    Made app scalable and implemented 2-tier in k8s

--- todolist-goof/todolist-core/src/main/resources/config/schema.sql
+++ todolist-goof/todolist-core/src/main/resources/config/schema.sql
@@ -2,10 +2,11 @@ CREATE TABLE IF NOT EXISTS user (
   id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name varchar(32) DEFAULT NULL,
   email varchar(32) NOT NULL,
-  password varchar(32) DEFAULT NULL
+  password varchar(32) DEFAULT NULL,
+  UNIQUE (email)
 );
 
-ALTER TABLE user ADD CONSTRAINT unique_email UNIQUE (email);
+-- ALTER TABLE user ADD CONSTRAINT unique_email UNIQUE (email);
 
 CREATE TABLE IF NOT EXISTS todo (
   id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -13,7 +14,8 @@ CREATE TABLE IF NOT EXISTS todo (
   title varchar(512) DEFAULT NULL,
   done boolean DEFAULT FALSE NOT NULL,
   priority tinyint NOT NULL,
-  due_date date DEFAULT NULL
+  due_date date DEFAULT NULL,
+  FOREIGN KEY (aut_id) REFERENCES user(id)
 );
 
-alter table todo add constraint user_fk foreign key (user_id) references user(id);
+-- alter table todo add constraint user_fk foreign key (user_id) references user(id);
